2d1e426e99516369e8378837fa4511387414bca7
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2018-11-04 12:35:59 -0800

    Added MongoDB and User model

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+// Initalize
 const express = require("express")
 const app = express()
+const exphbs = require("express-handlebars")
+const mongoose = require('mongoose');
 
-app.get("/", function (req, res) {
-  res.send("Hello World")
+// Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+mongoose.connect('mongodb://localhost/hard-of-hearing')
+
+// // Mock array for testing
+// let users = [
+//   { name: "Alex", bio: "Hard of hearing ally"},
+//   { name: "Bert", bio: "Loves Korean food"}
+// ]
+
+// Models
+const User = mongoose.model("User", {
+  name: String,
+  bio: String
 })
+
+app.get("/users", function (req, res) {
+  res.render("users-index", { users: users});
+})
+
+
 
 app.listen(process.env.PORT || 3000, (req, res) => {
   console.log("Listening at port 3000!")
